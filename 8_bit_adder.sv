d17203ed3825b4133d83f38449ea26a2ae0e07e7
ClssEdmilson <edmilsonclss@gmail.com>
2021-10-30 17:51:08 -0300

    Generator

--- 8_bit_adder.sv
+++ 8_bit_adder.sv
@@ -18,3 +18,24 @@ class transaction;
     bit [8:0] y;
 endclass //transaction
 
+//Generator class
+class generator;
+    mailbox mbx;
+    event done;
+    transaction t;
+    integer i;    
+
+    function new(mailbox mbx);
+        this.mbx = mbx;
+    endfunction //new()
+
+    task run();
+        t = new();
+        for ( i = 0; i < 50; i++) begin
+            t.randomize();
+            mbx.put(t);
+            $display(" [ GEN ] - Generator send the data.");
+            @(done);
+        end
+    endtask 
+endclass //generator
